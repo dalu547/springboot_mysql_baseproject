03be067561fa3dba0b1b79056f8a354e098765f9
Mivada <dalayya.kalla@mivada>
2025-09-02 16:50:08 +0530

    removed duplicate migration related files

--- src/main/resources/db/migration/V1__insert_product_table.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO product (id, name, price) VALUES
-                                          (UUID_TO_BIN(UUID()), 'Laptop', 1200.00),
-                                          (UUID_TO_BIN(UUID()), 'Smartphone', 800.00),
-                                          (UUID_TO_BIN(UUID()), 'Headphones', 150.00);
-
